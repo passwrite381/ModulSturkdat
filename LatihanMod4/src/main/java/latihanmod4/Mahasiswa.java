ccd805aac1aa36e9974dea9a9a6f398e676519e2
Diaz Vloderizz <127311341+passwrite381@users.noreply.github.com>
2023-04-12 15:22:19 +0700

    Add files via upload

---
+++ LatihanMod4/src/main/java/latihanmod4/Mahasiswa.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package latihanmod4;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ *
+ * @author diazh
+ */
+public class Mahasiswa {
+    String nama, kelas, matkulPraktikum;
+    int nim;
+    
+    public Mahasiswa(String nm, String kl, String mat, int ni){
+        nama = nm;
+        kelas = kl;
+        matkulPraktikum = mat;
+        nim = ni;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        HashMap<String, Mahasiswa> mhs = new HashMap<>();
+        String input;
+        Mahasiswa data;
+        
+        mhs.put("1", new Mahasiswa("Putri", "3H", "Struktur Data", 2020000));
+        mhs.put("2", new Mahasiswa("Agus", "3A", "Matematika", 2020012));
+        mhs.put("3", new Mahasiswa("Arro", "3D", "Pemrograman", 2020017));
+        
+        System.out.println("Masukkan ID : ");
+        input = in.nextLine();
+        data = mhs.get(input);
+        if (data != null){
+            System.out.println("Data Mahasiswa : " + data.nama + ", kelas : " + data.kelas + "mata kuliah praktikum : " + data.matkulPraktikum + ", nim : " + data.nim);
+        }
+    }
+}
